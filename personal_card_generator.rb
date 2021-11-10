a1d6eaab72743f2fdc952fd86a24ac54551fb469
radoxubuntu <trans4mer72@gmail.com>
2021-11-10 15:37:31 +0300

    создание <body> вынесено в отдельный метод create_body

--- personal_card_generator.rb
+++ personal_card_generator.rb
@@ -3,4 +3,4 @@ require_relative "personal_card_class"
 puts "Вы должны ввести данные для заполнения визитной карточки"
 
 personal_card = PersonalCard.new
-personal_card.create_html
+personal_card.create_html_doc

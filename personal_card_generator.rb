ce214a66376c945d103656d579d4436346cbf8fa
radoxubuntu <trans4mer72@gmail.com>
2021-11-10 11:23:09 +0300

    добавлен метод get_image

--- personal_card_generator.rb
+++ personal_card_generator.rb
@@ -3,4 +3,4 @@ require_relative "methods"
 puts "Вы должны ввести данные для заполнения визитной карточки"
 
 data = []
-data << get_name << get_description << get_phone << get_email
+data << get_image << get_name << get_description << get_phone << get_email
